ae9499be602a10f5493843d335edc53ac5aade2f
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-13 00:48:23 +0700

    menambahkan method pada app

--- app/core/App.php
+++ app/core/App.php
@@ -17,6 +17,15 @@ class App{
 
         require_once '../app/controllers' . $this->controller . '.php';
         $this->controller = new $this->controller;
+
+        //method
+        if(isset($url[1])){
+            if(method_exists($this->controller, $url[1])){
+                $this->method = $url[1];
+                unset($url[1]);
+
+            }
+        }
     }
 
     public function parseURL(){
