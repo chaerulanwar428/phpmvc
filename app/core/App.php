8a22c5e00f7d3cc8262da578dd44ff0cf698af4a
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-13 00:45:14 +0700

    menammbahkan controller pada app

--- app/core/App.php
+++ app/core/App.php
@@ -4,11 +4,19 @@ class App{
     protected $controller = 'Home';
     protected $method = 'index';
     protected $params = [];
-    
+
     public function __construct()
     {
         $url = $this->parseURL();
-        var_dump($url);
+        
+        //controller
+        if(file_exists('../app/controllers/' . $url[0] . '.php')){
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers' . $this->controller . '.php';
+        $this->controller = new $this->controller;
     }
 
     public function parseURL(){
