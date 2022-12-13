ead77701c13423853d8cb0620e5e15c0176b36d8
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-14 04:38:42 +0700

    menambahkan logika view about

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,12 +1,12 @@
 <?php
 
-class About{
+class About extends Controller{
     public function index($nama = 'Chaerul',$pekerjaan = 'Karyawan' , $umur = 32)
     {
-        echo "Halo,nama saya $nama , saya adalah seorang $pekerjaan. Saya berumur $umur tahun.";
+       $this->view('about/index', $data);
     }
     public function page()
     {
-        echo 'About/page';
+        $this->view('aboot/page');
     }
 }
