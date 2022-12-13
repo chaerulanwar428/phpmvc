a9ef048ca9117801d6c7f1d7f779a2aff0809092
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-14 02:50:38 +0700

    menambahkan beberapa diabout

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,9 +1,9 @@
 <?php
 
 class About{
-    public function index($nama = 'Chaerul',$pekerjaan = 'Karyawan')
+    public function index($nama = 'Chaerul',$pekerjaan = 'Karyawan' , $umur = 32)
     {
-        echo "Halo,nama saya $nama , saya adalah seorang $pekerjaan";
+        echo "Halo,nama saya $nama , saya adalah seorang $pekerjaan. Saya berumur $umur tahun.";
     }
     public function page()
     {
