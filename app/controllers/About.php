1e4565b3f2eb119dc5b91b7ccfe1ea52e0080a69
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-14 04:44:06 +0700

    supaya aboutbisa menangkap data

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -3,6 +3,9 @@
 class About extends Controller{
     public function index($nama = 'Chaerul',$pekerjaan = 'Karyawan' , $umur = 32)
     {
+        $data['nama'] = $nama;
+        $data ['pekerjaan'] = $pekerjaan;
+        $data ['umur'] = $umur;
        $this->view('about/index', $data);
     }
     public function page()
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>About me</h1>
+    <p>Halo, nama saya <?= $data['nama'];?> , umur saya <?= $data['umur'];?> tahun, saya adalah seorang <?= $data['pekerjaan'];?></p>
 </body>
 </html>
