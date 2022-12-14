2968817df2f188ebbc24f30156c7dbefcc8bca88
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-14 19:33:42 +0700

    menambahkan controller di home

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,6 +5,7 @@ class Home extends Controller {
     public function index()
     {
         $data['judul'] = 'Home';
+        $data['nama'] = $this->model('User_model')->getUser();
         $this->view('templates/header');
         $this-> view('home/index');
         $this->view('templates/footer');
