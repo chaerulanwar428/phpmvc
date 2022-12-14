605967bbc7c60f8961bdeb770fed6c8382b1385d
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-15 05:41:42 +0700

    menambahkan beberapa fitur di page mahasiswa

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -2,13 +2,13 @@
     <div class="row">
         <div class="col-06">
             <h3>Daftar Mahasiswa</h3>
+            <ul class="list-group">
             <?php foreach($data['mhs'] as $mhs) : ?>
-                <ul>
-                    <li><?= $mhs ['nama']; ?></li>
-                    <li><?= $mhs ['nim']; ?></li>
-                    <li><?= $mhs ['email']; ?></li>
-                    <li><?= $mhs ['jurusan']; ?></li>
-                </ul>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <?= $mhs['nama'];?>
+                <a href="<?= BASEURL;?>/mahasiswa/detail/<?= $mhs['id']?>" class="badge badge-primary">detail</a>
+            </li>
+            </ul>
             <?php endforeach; ?>
         </div>
     </div>
