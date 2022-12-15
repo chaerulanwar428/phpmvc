ffee5e740259f39a7bebed1b11b93a842598e3d8
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-16 04:54:14 +0700

    menambahkan tombol hapus dan alert

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -13,15 +13,21 @@
     <h3>Daftar Mahasiswa</h3>
             <ul class="list-group">
             <?php foreach($data['mhs'] as $mhs) : ?>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
+            <li class="list-group-item ">
                 <?= $mhs['nama'];?>
-                <a href="<?= BASEURL;?>/mahasiswa/detail/<?= $mhs['id']?>" class="badge badge-primary">detail</a>
+                <a href="<?= BASEURL;?>/mahasiswa/hapus/<?= $mhs['id']?>" class="badge badge-danger float-right ml-1" onclick="return confirm('yakin?');">hapus</a>
+                <a href="<?= BASEURL;?>/mahasiswa/detail/<?= $mhs['id']?>" class="badge badge-primary float-right ml-5">detail</a>
             </li>
             </ul>
             <?php endforeach; ?>
         </div>
     </div>
 </div>
+
+
+
+
+
 <!-- Modal -->
 <div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
   <div class="modal-dialog" role="document">
